4b7449cfd64b425e03b9b8ed9f4943572ee85d96
hrtk91 <ht91218@gmail.com>
2020-01-29 22:32:40 +0900

    DIContainer(Factory)追加

---
+++ FavColle/DIContainer/DI.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FavColle.DIContainer
+{
+    public class DI
+    {
+        protected static IDictionary<string, object> objects = new Dictionary<string, object>();
+
+        public static T Register<T>() where T : class, new()
+        {
+            var obj = new T();
+            objects.Add(typeof(T).Name, obj);
+
+            return obj;
+        }
+
+
+        public static T Get<T>() where T : class, new()
+        {
+            return (T)objects.Where(pair => pair.Key == typeof(T).Name).First().Value;
+        }
+    }
+}
